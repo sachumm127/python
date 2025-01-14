9e08a9aca15f874e8fdfe667614ee37cb3057968
sachin.m.m <82070128+sachumm127@users.noreply.github.com>
2025-01-14 13:58:27 +0530

    Update server.py

--- src/server.py
+++ src/server.py
@@ -9,4 +9,4 @@ def hello():
 
 if __name__ == "__main__":
 	app.config['TEMPLATES_AUTO_RELOAD'] = True
-	app.run(host='0.0.0.0', debug=True, port=8080)
+	app.run(host='0.0.0.0', debug=True, port=9090)
